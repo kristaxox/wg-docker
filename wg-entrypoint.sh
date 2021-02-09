d70e3543145fbf407cb5b55d8f3588daf99056e6
Krista Spriggs <krista@storj.io>
2021-02-09 16:04:29 -0500

    Tweak the entrypoint script + readme fixes

--- wg-entrypoint.sh
+++ wg-entrypoint.sh
@@ -12,4 +12,5 @@ trap cleanup INT HUP SIGINT
 echo "starting wg interface..."
 sysctl -w net.ipv4.ip_forward=1 && sysctl -w net.ipv4.conf.all.forwarding=1
 wg-quick up ${wgConfigPath}
-while true; do wg show; sleep 5; done
+wg show
+read # block!
